3bddef9f474fa0a1e5325971a50585fcccca5a03
Kuesters, Elmar <elmar.kuesters@sap.com>
2016-01-27 18:16:41 +0100

    changed yahoo api to https

--- yahooFinanceAPI/js/stockQuotesAPI.js
+++ yahooFinanceAPI/js/stockQuotesAPI.js
@@ -6,7 +6,7 @@ sap.ui.define(["sap/ui/base/Object"], function(BaseObject) {
 		fetchData : function(aSymbols, fSuccess) {
 			// var aSymbols = ["YHOO","AAPL","GOOG","MSFT"];
 			var sSymbols = '"' + aSymbols.join('","') + '"';
-			var url = 'http://query.yahooapis.com/v1/public/yql';
+			var url = 'https://query.yahooapis.com/v1/public/yql';
 			var data = encodeURIComponent('select * from yahoo.finance.quotes where symbol in (' + sSymbols + ')');
 			$.getJSON(url, 'q=' + data + "&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json", fSuccess);
 		}
